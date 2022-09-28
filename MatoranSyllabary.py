2e7b9c15d398226f146de3a4795ddeb9dc16484c
Kai Kuehner <kvk@pobox.com>
2022-09-27 22:34:10 -0400

    all letters supported

--- MatoranSyllabary.py
+++ MatoranSyllabary.py
@@ -2,15 +2,6 @@ import sys
 from turtle import width
 from PIL import Image, ImageDraw
 
-"""
-TODO:
-draw vowels
-draw consonants
-support full words
-support aspirated consonants
-support codas
-"""
-
 TRANSPARENT = (0, 0, 0, 0)
 BG_COLOR = (240, 240, 240)
 LINE_COLOR = (16, 16, 16)
@@ -18,7 +9,7 @@ FILL_COLOR = BG_COLOR
 SIZE = 128
 SPACING = 10
 BASE_WIDTH = 10
-CONSONANT_WIDTH = 8
+CONSONANT_WIDTH = 7
 CONSONANT_WIDTH_ASPIRATED = 10
 CONSONANT_GAP_ASPIRATED = 4
 VOWEL_WIDTH = 5
@@ -141,7 +132,7 @@ LETTERS = {
         "x2": 0,
         "y2": FULL_DIST
     }],
-    "k": [{
+    "g": [{
         "type": "line",
         "x1": -FULL_DIST,
         "y1": 0,
@@ -154,6 +145,20 @@ LETTERS = {
         "y1": -FULL_DIST,
         "x2": 0,
         "y2": FULL_DIST
+    }],
+    "k": [{
+        "type": "line",
+        "x1": -CORNER_DIST,
+        "y1": CORNER_DIST,
+        "x2": CORNER_DIST,
+        "y2": -CORNER_DIST
+        },
+        {
+        "type": "line",
+        "x1": -CORNER_DIST,
+        "y1": -CORNER_DIST,
+        "x2": CORNER_DIST,
+        "y2": CORNER_DIST
     }],
     "n": [{
         "type": "line",
@@ -182,6 +187,198 @@ LETTERS = {
         "y1": -NM_DIST_2,
         "x2": NM_DIST_1,
         "y2": NM_DIST_2
+    }],
+    "ch": [{
+        "type": "line",
+        "x1": 0,
+        "y1": 0,
+        "x2": CORNER_DIST,
+        "y2": CORNER_DIST
+        },
+        {
+        "type": "line",
+        "x1": -CORNER_DIST,
+        "y1": CORNER_DIST,
+        "x2": CORNER_DIST,
+        "y2": -CORNER_DIST
+    }],
+    "j": [{
+        "type": "line",
+        "x1": 0,
+        "y1": 0,
+        "x2": -CORNER_DIST,
+        "y2": CORNER_DIST
+        },
+        {
+        "type": "line",
+        "x1": -CORNER_DIST,
+        "y1": -CORNER_DIST,
+        "x2": CORNER_DIST,
+        "y2": CORNER_DIST
+    }],
+    "v": [{
+        "type": "line",
+        "x1": 0,
+        "y1": 0,
+        "x2": -CORNER_DIST,
+        "y2": -CORNER_DIST
+        },
+        {
+        "type": "line",
+        "x1": -CORNER_DIST,
+        "y1": CORNER_DIST,
+        "x2": CORNER_DIST,
+        "y2": -CORNER_DIST
+    }],
+    "f": [{
+        "type": "line",
+        "x1": 0,
+        "y1": 0,
+        "x2": CORNER_DIST,
+        "y2": -CORNER_DIST
+        },
+        {
+        "type": "line",
+        "x1": -CORNER_DIST,
+        "y1": -CORNER_DIST,
+        "x2": CORNER_DIST,
+        "y2": CORNER_DIST
+    }],
+    "r": [{
+        "type": "line",
+        "x1": 0,
+        "y1": 0,
+        "x2": FULL_DIST,
+        "y2": 0
+        },
+        {
+        "type": "line",
+        "x1": 0,
+        "y1": 0,
+        "x2": 0,
+        "y2": FULL_DIST
+        },
+        {
+        "type": "center-rect"
+    }],
+    "w": [{
+        "type": "line",
+        "x1": 0,
+        "y1": 0,
+        "x2": -FULL_DIST,
+        "y2": 0
+        },
+        {
+        "type": "line",
+        "x1": 0,
+        "y1": 0,
+        "x2": 0,
+        "y2": FULL_DIST
+        },
+        {
+        "type": "center-rect"
+    }],
+    "th": [{
+        "type": "line",
+        "x1": 0,
+        "y1": 0,
+        "x2": -FULL_DIST,
+        "y2": 0
+        },
+        {
+        "type": "line",
+        "x1": 0,
+        "y1": 0,
+        "x2": 0,
+        "y2": -FULL_DIST
+        },
+        {
+        "type": "center-rect"
+    }],
+    "x": [{
+        "type": "line",
+        "x1": 0,
+        "y1": 0,
+        "x2": FULL_DIST,
+        "y2": 0
+        },
+        {
+        "type": "line",
+        "x1": 0,
+        "y1": 0,
+        "x2": 0,
+        "y2": -FULL_DIST
+        },
+        {
+        "type": "center-rect"
+    }],
+    "br": [{
+        "type": "line",
+        "x1": 0,
+        "y1": 0,
+        "x2": CORNER_DIST,
+        "y2": -CORNER_DIST
+        },
+        {
+        "type": "line",
+        "x1": 0,
+        "y1": 0,
+        "x2": CORNER_DIST,
+        "y2": CORNER_DIST
+        },
+        {
+        "type": "center-diamond"
+    }],
+    "kr": [{
+        "type": "line",
+        "x1": 0,
+        "y1": 0,
+        "x2": -CORNER_DIST,
+        "y2": -CORNER_DIST
+        },
+        {
+        "type": "line",
+        "x1": 0,
+        "y1": 0,
+        "x2": -CORNER_DIST,
+        "y2": CORNER_DIST
+        },
+        {
+        "type": "center-diamond"
+    }],
+    "fr": [{
+        "type": "line",
+        "x1": 0,
+        "y1": 0,
+        "x2": -CORNER_DIST,
+        "y2": -CORNER_DIST
+        },
+        {
+        "type": "line",
+        "x1": 0,
+        "y1": 0,
+        "x2": CORNER_DIST,
+        "y2": -CORNER_DIST
+        },
+        {
+        "type": "center-diamond"
+    }],
+    "tr": [{
+        "type": "line",
+        "x1": 0,
+        "y1": 0,
+        "x2": -CORNER_DIST,
+        "y2": CORNER_DIST
+        },
+        {
+        "type": "line",
+        "x1": 0,
+        "y1": 0,
+        "x2": CORNER_DIST,
+        "y2": CORNER_DIST
+        },
+        {
+        "type": "center-diamond"
     }]
 }
 
@@ -212,6 +409,10 @@ def draw_letter(letter, draw, x, affix_to, aspirated):
                 aspiration_queue.append((x1, y1, x2, y2))
             else:
                 draw.line((x1, y1, x2, y2), fill=LINE_COLOR, width=CONSONANT_WIDTH)
+        elif shape["type"] == "center-rect":
+            draw.rectangle((cx - CONSONANT_WIDTH // 2, cy - CONSONANT_WIDTH // 2, cx + CONSONANT_WIDTH / 2, cy + CONSONANT_WIDTH / 2), fill=LINE_COLOR)
+        elif shape["type"] == "center-diamond":
+            draw.regular_polygon((cx, cy, CONSONANT_WIDTH / 2), 4, rotation=45, fill=LINE_COLOR)
     for line in aspiration_queue:
         draw.line(line, fill=BG_COLOR, width=CONSONANT_GAP_ASPIRATED)
 
@@ -234,6 +435,9 @@ def main():
             elif last_seen_vowel is not None:
                 affix_to = last_seen_vowel
             aspirated = False
+            if j + 1 < len(syllable) and (letter + syllable[j + 1]) in LETTERS:
+                letter = letter + syllable[j + 1]
+                j += 1
             if j + 1 < len(syllable) and syllable[j + 1] == "h":
                 aspirated = True
                 j += 1
